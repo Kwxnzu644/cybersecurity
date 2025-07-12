ed4f5d7c4470c6fdfee677a51a8b91890575c9a4
Alvin Kwanzu <alvintrvs@gmail.com>
2025-07-12 19:58:19 +0300

    Add files via upload

---
+++ python/00_basics/README.md
@@ -0,0 +1,26 @@
+# 🐍 Python Week 1: Fundamentals for Cybersecurity
+
+This folder contains foundational Python scripts designed to help master the **core basics of scripting for cybersecurity**.
+
+## ✅ What  Learned
+
+- **Input/output**: Getting data from the user or reading from files
+- **Functions**: Creating reusable logic blocks for my tools
+- **File handling**: Writing to logs or reading scan results
+- **CLI arguments**: Using `argparse` to build real command-line tools
+
+---
+
+## 📁 Scripts in this Folder
+
+### `hello_banner.py`
+A simple command-line script that prints a message in a decorated format.
+
+**Key Concepts:**
+- `argparse` to get message input from CLI
+- `def` to define a reusable `print_banner()` function
+- String manipulation
+
+**Example:**
+```bash
+python3 hello_banner.py -m "Welcome to Secverse"
---
+++ python/00_basics/clibanner.py
@@ -0,0 +1,27 @@
+import argparse
+
+def banner(message, style):
+    if style == "plain":
+        print( "+" + "-" *  len(message) + "+")
+        print(message)
+        print( "+" + "-" *  len(message) + "+")
+    elif style == "equals":
+        print("=" * len(message))
+        print(message)
+        print("=" * len(message))
+    elif style == "box":
+        print("|" + "_" * len(message)+"|")
+        print(message)
+        print("|" + "_" * len(message)+"|")
+    else:
+        print("Enter a valid style")
+
+parser=argparse.ArgumentParser(description="this is a style banner")
+
+parser.add_argument("-m","--message", required=True , help="display the banner")
+
+parser.add_argument("-s","--style", choices=["plain","box","equals"], default="plain" , help="diplay Styled Banner")
+
+args=parser.parse_args()
+
+banner(args.message,args.style)
