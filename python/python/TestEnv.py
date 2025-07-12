dae640c28b545b312de37fb2b9a86d1b73ea15e4
Alvin Kwanzu <alvintrvs@gmail.com>
2025-07-12 12:41:48 +0300

    644

--- python/python/TestEnv.py
+++
@@ -1,9 +0,0 @@
-import json
-
-data = {"name": "kwanzu", "age": 20}
-with open('test.json','w')as f:
-   json.dump(data, f)
-
-with open('test.json', 'r') as f:
-    data = json.load(f)
-    print(data)
